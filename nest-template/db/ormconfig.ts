3088b8a80b6296fc42e4d1d6036aabd61f96e891
Tulio <tulio-santos@hotmail.com>
2023-10-27 20:50:35 -0300

    template - change orm entity import

--- nest-template/db/ormconfig.ts
+++ nest-template/db/ormconfig.ts
@@ -1,5 +1,6 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import 'dotenv/config';
+import { User } from '../src/auth/entities';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
@@ -8,7 +9,7 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: ['dist/**/*.entity.js'],
+  entities: [User],
   migrations: ['dist/db/migrations/*.js'],
 };
 
